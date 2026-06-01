45c657522820b1fd795fb156ebdb4ec4b229e9fb
jana hussein <jana2400036@miuegypt.edu.eg>
2026-06-01 12:56:54 +0000

    change

--- routes/admin.js
+++ routes/admin.js
@@ -7,25 +7,25 @@ const {
   getAllBookings, deleteReview,
 } = require('../controllers/admin.controller');
 
-// All admin routes: must be logged in AND be admin
+
 router.use(protect, authorize('admin'));
 
 router.get('/dashboard',          getDashboard);
 
-// Users
+
 router.get('/users',              getAllUsers);
 router.put('/users/:id/role',     updateUserRole);
 router.put('/users/:id/toggle',   toggleUserStatus);
 
-// Places
+
 router.get('/places',             getAllPlaces);
 router.put('/places/:id/verify',  verifyPlace);
 router.delete('/places/:id',      adminDeletePlace);
 
-// Bookings
+
 router.get('/bookings',           getAllBookings);
 
-// Reviews
+
 router.delete('/reviews/:id',     deleteReview);
 
 module.exports = router;
