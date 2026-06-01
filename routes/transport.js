ce564fae02520f405f04b38f66e02ea0fb046685
jana hussein <jana2400036@miuegypt.edu.eg>
2026-06-01 12:57:23 +0000

    change

--- routes/transport.js
+++ routes/transport.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const { protect, authorize } = require('../middleware/auth');
 const { getStations, getRoutes, getSeatsAvailability, updateRoutes } = require('../controllers/transport.controller');
 
-// Public
+
 router.get('/stations',                               getStations);
 router.get('/stations/:id/routes',                    getRoutes);
 router.get('/stations/:id/routes/:routeIndex/seats',  getSeatsAvailability);
 
-// Owner / admin only
+
 router.put('/stations/:id/routes', protect, authorize('business_owner', 'admin'), updateRoutes);
 
 module.exports = router;
