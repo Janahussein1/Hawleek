a7c5b090a76305847057f7456109c6a26507f921
kareem2406142 <kareem2406142@miuegypt.edu.eg>
2026-06-08 15:15:50 +0300

    user.js

--- routes/user.js
+++ routes/user.js
@@ -17,24 +17,4 @@ router.get('/bookings',      getMyBookings);
 router.get('/my-reviews',    getMyReviews);
 router.delete('/account',    deleteAccount);
 
-
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-const { uploadSingle } = require('../middleware/upload');
-const {
-  getProfile, updateProfile, uploadAvatar,
-  getMyBookings, getMyReviews, deleteAccount,
-} = require('../controllers/cuser');
-
-router.use(protect); // All user routes require auth
-
-router.get('/profile',       getProfile);
-router.put('/profile',       updateProfile);
-router.put('/avatar',        uploadSingle('avatar'), uploadAvatar);
-router.get('/my-bookings',   getMyBookings);
-router.get('/bookings',      getMyBookings);
-router.get('/my-reviews',    getMyReviews);
-router.delete('/account',    deleteAccount);
-
 module.exports = router;
